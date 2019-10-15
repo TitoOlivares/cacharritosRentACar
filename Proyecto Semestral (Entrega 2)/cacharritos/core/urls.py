43a38b5139782be2bd760b4ef7425cbf08cf5ae6
Jose Antonio Gallegos <jo.gallegosc@alumnos.duoc.cl>
2019-10-15 09:49:17 -0300

    Revert "Entrega 2 (Migración del sitio original al framework Django)"
    This reverts commit 590923629a91727c93bbd0da7bede037c7669ac8.

--- Proyecto Semestral (Entrega 2)/cacharritos/core/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import home, vehiculos, contacto
-
-urlpatterns = [    
-    path('', home, name="home"),
-    path('vehiculos/', vehiculos, name="vehiculos"),
-    path('contacto/', contacto, name="contacto"),
-]
