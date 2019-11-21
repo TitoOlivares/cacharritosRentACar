d6244ee4ba246ae5db49108f4921a13fbae702fe
Felipe <f.astudilloo@alumnos.duoc.cl>
2019-11-21 19:24:00 -0300

    pruebas  unitarias

--- Proyecto Semestral (Entrega 2)/cacharritos/core/tests.py
+++ Proyecto Semestral (Entrega 2)/cacharritos/core/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+from django.test import TestCase,Client
+from django.contrib.auth.models import User  ## usuario
+from .forms import CustomUserForm
+from django.urls import reverse
+
 
 # Create your tests here.
+
+
+class gestion_Usuario(TestCase):
+    def setUp (self):
+        self.cliente = Client()
+        User.objects.create_user('Li',password= 'adminadmin')
+    
+
+
+    def test_Login(self):
+        response= self.cliente.post(
+            reverse('login'),{'username':'Li','password':'adminadmin'})
+        self.assertEqual(response.status_code,302,'prueba exitosa')
+
+
+
+    def test_Login_Fail(self):
+        response= self.cliente.post(
+            reverse('login'),{'username':'Li','password':'admin'})
+        self.assertNotEqual(response.status_code,302,'prueba exitosa')    
+
---
+++ Proyecto Semestral (Entrega 2)/readme.txt
@@ -0,0 +1,14 @@
+
+output p. unitarias
+
+C:\Users\E-tec\Desktop\Proyecto Semestral (Entrega 2)\cacharritos>manage.py test
+Creating test database for alias 'default'...
+System check identified no issues (0 silenced).
+..
+----------------------------------------------------------------------
+Ran 2 tests in 0.694s
+
+OK
+Destroying test database for alias 'default'...
+
+C:\Users\E-tec\Desktop\Proyecto Semestral (Entrega 2)\cacharritos>
